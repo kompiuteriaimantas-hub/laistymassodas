56eb0e475007e6f7fc28418fd88289d7cf73839c
kompiuteriaimantas-hub <kompiuteriai.mantas@gmail.com>
2026-06-05 16:49:10 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ import wateringCommand from "./routes/wateringCommand.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api/watering/command", wateringCommand);
+
+// STATIC FAILAI TURI BŪTI PRIEŠ API ROUTES
 app.use(express.static("public"));
 
-
+// API ROUTES
+app.use("/api/watering/command", wateringCommand);
 app.use("/api/sensors", sensors);
 app.use("/api/schedules", schedules);
-app.use("/api/watering", watering); // VIENINTELIS watering route
+app.use("/api/watering", watering);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("API veikia ant porto:", port));
